371377ef1153c93fbe066d107db62b01437f9919
Amir Karimi <a.karimi.k@gmail.com>
2013-06-30 12:49:16 +0430

    Clean UrlHelper.
    - Add license.
    - Fix comments.
    - Rename UrlHelper method.

--- Source/Xoqal.Utilities/UrlHelper.cs
+++ Source/Xoqal.Utilities/UrlHelper.cs
@@ -1,9 +1,27 @@
-﻿namespace Xoqal.Utilities
+﻿#region License
+// UrlHelper.cs
+// 
+// Copyright (c) 2012 Xoqal.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+namespace Xoqal.Utilities
 {
     using System.Text.RegularExpressions;
 
     /// <summary>
-    ///     provide some helpers static method toward of url
+    /// Provide some helpers static method toward of URL.
     /// </summary>
     public static class UrlHelper
     {
@@ -15,43 +33,40 @@
         private static readonly Regex Percent = new Regex(@"([0-9]+)%", RegexOptions.Compiled);
         private static readonly Regex Sep = new Regex(@"[\s_/\\+:.]", RegexOptions.Compiled);
 
-        // latin char(s)
-        //private static readonly Regex Empty = new Regex(@"[^-A-Za-z0-9]", RegexOptions.Compiled);
-
-        // included persian char(s)
+        // included Persian char(s)
         private static readonly Regex Empty = new Regex(@"[^-A-Za-z0-9\u0600-\u06FF]", RegexOptions.Compiled);
         private static readonly Regex Extra = new Regex(@"[-]+", RegexOptions.Compiled);
 
         /// <summary>
-        ///     get a string, prepare(clean) it for using in url
+        /// Gets readable encoded URL from the specified raw URL.
         /// </summary>
-        /// <param name="str"></param>
+        /// <param name="rawUrl"></param>
         /// <returns></returns>
-        /// <remarks>persian characters included</remarks>
-        public static string PrepareUrl(string str)
+        /// <remarks>Persian characters included.</remarks>
+        public static string GetReadableEncodedUrl(string rawUrl)
         {
-            if (!string.IsNullOrEmpty(str))
+            if (!string.IsNullOrEmpty(rawUrl))
             {
-                str = str.Trim().ToLower();
-                str = str.Replace("&", "and");
+                rawUrl = rawUrl.Trim().ToLower();
+                rawUrl = rawUrl.Replace("&", "and");
 
-                str = Feet.Replace(str, "$1-ft-");
-                str = Inch1.Replace(str, "$1-in-");
-                str = Inch2.Replace(str, "$1-in-");
-                str = Num.Replace(str, "num-$1");
+                rawUrl = Feet.Replace(rawUrl, "$1-ft-");
+                rawUrl = Inch1.Replace(rawUrl, "$1-in-");
+                rawUrl = Inch2.Replace(rawUrl, "$1-in-");
+                rawUrl = Num.Replace(rawUrl, "num-$1");
 
-                str = Dollar.Replace(str, "$1-dollar-");
-                str = Percent.Replace(str, "$1-percent-");
+                rawUrl = Dollar.Replace(rawUrl, "$1-dollar-");
+                rawUrl = Percent.Replace(rawUrl, "$1-percent-");
 
-                str = Sep.Replace(str, "-");
+                rawUrl = Sep.Replace(rawUrl, "-");
 
-                str = Empty.Replace(str, string.Empty);
-                str = Extra.Replace(str, "-");
+                rawUrl = Empty.Replace(rawUrl, string.Empty);
+                rawUrl = Extra.Replace(rawUrl, "-");
 
-                str = str.Trim('-');
+                rawUrl = rawUrl.Trim('-');
             }
 
-            return str;
+            return rawUrl;
         }
     }
 }
