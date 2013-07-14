5d0340e130fc637b7c1103c7c1a5523e557b58d8
Amir Karimi <a.karimi.k@gmail.com>
2013-07-14 22:55:22 +0430

    Added StringExtensions.RemoveIfEndsWith method.

---
+++ Source/Xoqal.Tests/UtilitiesTests/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Xoqal.Tests.UtilitiesTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Xoqal.Utilities.Extensions;
+
+    [TestClass]
+    public class StringExtensions
+    {
+        [TestMethod]
+        public void RemoveIfEndsWithReturnsCorrectResults()
+        {
+            // Arrange
+            var source1 = "TestApple";
+            var source2 = "Test";
+            var text = "Apple";            
+
+            // Act
+            var source1Result = source1.RemoveIfEndsWith(text);
+            var source2Result = source2.RemoveIfEndsWith(text);
+
+            // Assert
+            Assert.AreEqual("Test", source1Result);
+            Assert.AreEqual("Test", source2Result);
+        }
+
+        [TestMethod]
+        public void RemoveIfEndsWithWorksWithEmptyStrings()
+        {
+            string.Empty.RemoveIfEndsWith("Test");
+            string.Empty.RemoveIfEndsWith(string.Empty);
+        }
+    }
+}
